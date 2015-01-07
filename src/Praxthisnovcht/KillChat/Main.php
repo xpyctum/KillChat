684c643c3d8b7e56d6de892f59338b809620820e
Praxthisnovcht <praxthisnovcht@gmail.com>
2015-01-07 01:04:23 +0100

    Update Main.php

--- src/Praxthisnovcht/KillChat/Main.php
+++ src/Praxthisnovcht/KillChat/Main.php
@@ -1,3 +1,39 @@
 <?php
 // Addon for CustomChat
 namespace Praxthisnovcht\KillChat;
+
+use pocketmine\IPlayer;
+use pocketmine\Player;
+use pocketmine\Server;
+use pocketmine\plugin\PluginBase;
+use pocketmine\utils\Config;
+
+
+use pocketmine\event\player\PlayerDeathEvent;
+use pocketmine\event\Listener;
+
+class KillChat extends PluginBase implements Listener{
+
+    public function onEnable(){
+		$Name = $user->getPlayer()->getName();		
+		if(!(file_exists($this->plugin->getDataFolder() . "Counter/" . strtolower($Name) . ".yml")))
+		{
+			return new Config($this->plugin->getDataFolder() . "Counter/" . strtolower($Name) . ".yml", Config::YAML, array(
+				"Name" => $Name,
+				"Kills" => $this->plugin->getMurderDone()->getName(),
+				"Deaths" => $this->plugin->getDeathsDone()->getName(),
+				),
+			));
+		}
+		else
+		{
+			return new Config($this->plugin->getDataFolder() . "Counter/" . strtolower($Name) . ".yml", Config::YAML, array(
+			));
+		}
+	     $this->saveDefaultConfig();
+		 $this->getLogger()->info("KillChat has been enabled.");
+	
+	}		
+
+
+           
