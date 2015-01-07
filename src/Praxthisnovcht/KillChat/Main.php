67cb3c8e5c306578c871fd6f076d1fecf84045ec
Samuel <Samuel_ipad2@hotmail.com>
2015-01-07 12:07:21 +1100

    Cleaned up Errors
    Debugged basic errors

--- src/Praxthisnovcht/KillChat/Main.php
+++ src/Praxthisnovcht/KillChat/Main.php
@@ -7,40 +7,31 @@ use pocketmine\Player;
 use pocketmine\Server;
 use pocketmine\plugin\PluginBase;
 use pocketmine\utils\Config;
-
-
 use pocketmine\event\player\PlayerDeathEvent;
 use pocketmine\event\Listener;
+use Praxthisnovcht\CustomChat;
 
-class KillChat extends PluginBase implements Listener{
+class KillChat extends PluginBase implements Listener {
 
     public function onEnable()
     {
-		$Name = $user->getPlayer()->getName();		
-		if(!(file_exists($this->plugin->getDataFolder() . "Counter/" . strtolower($Name) . ".yml")))
-		{
-			return new Config($this->plugin->getDataFolder() . "Counter/" . strtolower($Name) . ".yml", Config::YAML, array(
-				"Name" => $Name,
-				
-				
-				"Kills" => $this->plugin->getMurderDone()->getName(),
-				
-				
-				"Deaths" => $this->plugin->getDeathsDone()->getName(),
-				
-				
-				),
-			));
-		}
-		else
-		{
-			return new Config($this->plugin->getDataFolder() . "Counter/" . strtolower($Name) . ".yml", Config::YAML, array(
-			));
-		}
-	         $this->saveDefaultConfig();
-		 $this->getLogger()->info("KillChat has been enabled.");
-	
-	}		
+        $Name = $user->getPlayer()->getName();
+        if (!(file_exists($this->plugin->getDataFolder() . "Counter/" . strtolower($Name) . ".yml"))) {
+            return new Config($this->plugin->getDataFolder() . "Counter/" . strtolower($Name) . ".yml", Config::YAML, array(
+                    "Name" => $Name,
 
 
-           
+                    "Kills" => $this->plugin->getMurderDone()->getName(),
+
+
+                    "Deaths" => $this->plugin->getDeathsDone()->getName(),
+
+            ));
+        } else {
+            return new Config($this->plugin->getDataFolder() . "Counter/" . strtolower($Name) . ".yml", Config::YAML, array());
+        }
+        $this->saveDefaultConfig();
+        $this->getLogger()->info("KillChat has been enabled.");
+    }
+
+}
